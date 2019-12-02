9aa85653a41f75c56537a4ad595c4ac79c130adc
avarpow <648120201@qq.com>
2019-12-02 16:56:56 +0800

    controol 3 servo ok

--- examples/peripherals/mcpwm/mcpwm_servo_control/main/mcpwm_servo_control_example.c
+++ examples/peripherals/mcpwm/mcpwm_servo_control/main/mcpwm_servo_control_example.c
@@ -23,7 +23,9 @@
 static void mcpwm_example_gpio_initialize(void)
 {
     printf("initializing mcpwm servo control gpio......\n");
-    mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0A, 18);    //Set GPIO 18 as PWM0A, to which servo is connected
+    mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0A, 4);    //Set GPIO 18 as PWM0A, to which servo is connected
+    mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0B, 2);
+    mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM1A, 14);
 }
 
 /**
@@ -44,11 +46,9 @@ static uint32_t servo_per_degree_init(uint32_t degree_of_rotation)
 /**
  * @brief Configure MCPWM module
  */
-void mcpwm_example_servo_control(void *arg)
+void mcpwm_example_servo1_control(void *arg)
 {
     uint32_t angle, count;
-    //1. mcpwm gpio initialization
-    mcpwm_example_gpio_initialize();
 
     //2. initial mcpwm configuration
     printf("Configuring Initial Parameters of mcpwm......\n");
@@ -61,11 +61,56 @@ void mcpwm_example_servo_control(void *arg)
     mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_0, &pwm_config);    //Configure PWM0A & PWM0B with above settings
     while (1) {
         for (count = 0; count < SERVO_MAX_DEGREE; count++) {
-            printf("Angle of rotation: %d\n", count);
+            printf("SERVO 1 Angle of rotation: %d\n", count);
             angle = servo_per_degree_init(count);
             printf("pulse width: %dus\n", angle);
             mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, angle);
-            vTaskDelay(10);     //Add delay, since it takes time for servo to rotate, generally 100ms/60degree rotation at 5V
+            vTaskDelay(50 / portTICK_PERIOD_MS);     //Add delay, since it takes time for servo to rotate, generally 100ms/60degree rotation at 5V
+        }
+    }
+}
+void mcpwm_example_servo2_control(void *arg)
+{
+    uint32_t angle, count;
+
+    //2. initial mcpwm configuration
+    printf("Configuring Initial Parameters of mcpwm......\n");
+    mcpwm_config_t pwm_config;
+    pwm_config.frequency = 50;    //frequency = 50Hz, i.e. for every servo motor time period should be 20ms
+    pwm_config.cmpr_a = 0;    //duty cycle of PWMxA = 0
+    pwm_config.cmpr_b = 0;    //duty cycle of PWMxb = 0
+    pwm_config.counter_mode = MCPWM_UP_COUNTER;
+    pwm_config.duty_mode = MCPWM_DUTY_MODE_0;
+    mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_0, &pwm_config);    //Configure PWM0A & PWM0B with above settings
+    while (1) {
+        for (count = 0; count < SERVO_MAX_DEGREE/2; count++) {
+            printf("SERVO 2 Angle of rotation: %d\n", count);
+            angle = servo_per_degree_init(count);
+            printf("pulse width: %dus\n", angle);
+            mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_B, angle);
+            vTaskDelay(50 / portTICK_PERIOD_MS);     //Add delay, since it takes time for servo to rotate, generally 100ms/60degree rotation at 5V
+        }
+    }
+}
+void mcpwm_example_servo3_control(void *arg)
+{
+    uint32_t angle, count;
+    //2. initial mcpwm configuration
+    printf("Configuring Initial Parameters of mcpwm......\n");
+    mcpwm_config_t pwm_config;
+    pwm_config.frequency = 50;    //frequency = 50Hz, i.e. for every servo motor time period should be 20ms
+    pwm_config.cmpr_a = 0;    //duty cycle of PWMxA = 0
+    pwm_config.cmpr_b = 0;    //duty cycle of PWMxb = 0
+    pwm_config.counter_mode = MCPWM_UP_COUNTER;
+    pwm_config.duty_mode = MCPWM_DUTY_MODE_0;
+    mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_1, &pwm_config);    //Configure PWM0A & PWM0B with above settings
+    while (1) {
+        for (count = 0; count < SERVO_MAX_DEGREE/4; count++) {
+            printf("SERVO 3 Angle of rotation: %d\n", count);
+            angle = servo_per_degree_init(count);
+            printf("pulse width: %dus\n", angle);
+            mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_1, MCPWM_OPR_A, angle);
+            vTaskDelay(50 / portTICK_PERIOD_MS);    //Add delay, since it takes time for servo to rotate, generally 100ms/60degree rotation at 5V
         }
     }
 }
@@ -73,5 +118,9 @@ void mcpwm_example_servo_control(void *arg)
 void app_main(void)
 {
     printf("Testing servo motor.......\n");
-    xTaskCreate(mcpwm_example_servo_control, "mcpwm_example_servo_control", 4096, NULL, 5, NULL);
+    mcpwm_example_gpio_initialize();
+    xTaskCreate(mcpwm_example_servo1_control, "mcpwm_example_servo1_control", 4096, NULL, 5, NULL);
+    xTaskCreate(mcpwm_example_servo2_control, "mcpwm_example_servo2_control", 4096, NULL, 5, NULL);
+    xTaskCreate(mcpwm_example_servo3_control, "mcpwm_example_servo3_control", 4096, NULL, 5, NULL);
+
 }
